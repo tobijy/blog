5209778394499e4ef485fc344c777c76a99e0ef7
Tobias Vogel <mail@tobiasvogel.eu>
2012-05-13 23:48:51 +0200

    database

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
+from datetime import datetime
+
 from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(80))
+	content = db.Column(db.Text)
+	pub_date = db.Column(db.DateTime)
+	year = db.Column(db.Integer)
+	month = db.Column(db.Integer)
+	day = db.Column(db.Integer)
+
+	def __init__(self, title, content):
+		self.title = title
+		self.content = content
+		pub_date = datetime.utcnow()
+		self.pub_date = pub_date
+		self.year = pub_date.year
+		self.month = pub_date.month
+		self.day = pub_date.day
+
+	def __repr__(self):
+		return '< Post %r>' % self.title
+
+	
 
 @app.route('/')
 @app.route('/blog/')
 def blog():
-	return render_template('blog.html')
+	return render_template('blog.html', posts=Post.query.all())
 
 @app.route('/post/<int:year>/<int:month>/<path:title>/')
 def post(year,month, title):
